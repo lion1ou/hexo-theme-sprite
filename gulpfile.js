6f45ff3032f7f875a157eba936039b9ce290423c
luodezhong <luodezhong613@hellobike.com>
2021-09-29 19:35:05 +0800

    fix: 修改任务为gulp最新版

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,25 @@
 
 var gulp = require('gulp');
 var less = require('gulp-less');
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
-
 var LessAutoprefix = require('less-plugin-autoprefix');
+
 var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
-gulp.task('default', ['less'], function() {
-  gulp.watch('./source/less/*.less', ['less']);
-});
+function watch() {
+  gulp.watch('./source/_less/*.less', css);
+}
 
-gulp.task('less', function() {
-  return gulp.src('./source/less/xoxo.less')
-    .pipe(sourcemaps.init())
-    .pipe(less({
-      plugins: [autoprefix]
-     }))
-    .on('error', console.error.bind(console))
-    .pipe(sourcemaps.write('.'))
+function css(cb) {
+  return gulp
+    .src('./source/_less/main.less')
+    .pipe(less({ plugins: [autoprefix] }))
     .pipe(gulp.dest('./source/css/', { overwrite: true }));
-});
+}
 
-gulp.task('scripts', function() {
-  var files = [
-    './source/js/xoxo.js',
-  ];
+function js(cb) {
+  console.log('js...');
+  cb();
+}
 
-  return gulp.src(files)
-    .pipe(uglify())
-    .pipe(gulp.dest('./source/js'));
-});
+exports.default = gulp.series(css, js, watch);
+exports.build = gulp.series(css, js);
