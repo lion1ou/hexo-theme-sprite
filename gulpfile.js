69525f431f31d5465cfbafebafc05a45a36a1b67
lion1ou <lion1ou@163.com>
2021-10-10 17:31:04 +0800

    fix: 完善gulp工作流

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@
 
 var gulp = require('gulp');
 var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var LessAutoprefix = require('less-plugin-autoprefix');
 
 var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
 function watch() {
   gulp.watch('./source/_less/*.less', css);
+  gulp.watch('./source/_js/*.js', js);
 }
 
 function css(cb) {
@@ -18,8 +21,11 @@ function css(cb) {
 }
 
 function js(cb) {
-  console.log('js...');
-  cb();
+  return gulp
+    .src('./source/_js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./source/js/', { overwrite: true }));
 }
 
 exports.default = gulp.series(css, js, watch);
